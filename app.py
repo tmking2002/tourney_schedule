1ca52f0557316026d5570926ba0056f43d707640
tmking2002 <tmking4@ncsu.edu>
2024-06-05 19:10:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,25 +18,16 @@ if tourney == 'PGF Show Me The Money Showcase':
     teams = teams.unique()
     teams = sorted(teams)
 
-    teams = st.multiselect('Select Team', teams)
+    teams = st.multiselect('Select Teams', teams)
 
     filtered_data = filtered_data[(filtered_data['Away'].isin(teams)) | (filtered_data['Home'].isin(teams))]
+
     filtered_data['Time'] = pd.to_datetime(filtered_data['Date'] + '/24 ' + filtered_data['Time'])
+    filtered_data = filtered_data[filtered_data['Time'] > pd.Timestamp.now()]
+
     filtered_data = filtered_data[['Age Group', 'Away', 'Home', 'Time', 'Field']]
 
     filtered_data['Time'] = filtered_data['Time'].dt.strftime('%m/%d/%Y %I:%M %p')
     filtered_data = filtered_data.sort_values(by='Time')
 
-    if not filtered_data.empty:
-        fig = go.Figure(data=[go.Table(
-            header=dict(values=list(filtered_data.columns),
-                        fill_color='paleturquoise',
-                        align='left'),
-            cells=dict(values=[filtered_data[col] for col in filtered_data.columns],
-                    fill_color='white',
-                    align='left'))
-        ])
-        
-        st.plotly_chart(fig)
-    else:
-        st.write("No data available for the selected filters.")
+    st.table(filtered_data)
