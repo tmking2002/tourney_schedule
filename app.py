28e8061e75cb1be8beea00cc1915e496ac48b53a
tmking2002 <tmking4@ncsu.edu>
2024-06-05 19:14:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import plotly.graph_objects as go
 
 st.title('Tournament Schedule')
 
-tourney = st.selectbox('Select Tournament', ['PGF Show Me The Money Showcase'])
+tourney = st.sidebar.selectbox('Select Tournament', ['PGF Show Me The Money Showcase'])
 
 if tourney == 'PGF Show Me The Money Showcase':
 
     data = pd.read_csv('game_info.csv')
 
-    age_group = st.multiselect('Select Age Group', data['Age Group'].unique())
+    age_group = st.sidebar.multiselect('Select Age Group', data['Age Group'].unique())
 
     filtered_data = data[data['Age Group'].isin(age_group)]
 
@@ -18,14 +18,14 @@ if tourney == 'PGF Show Me The Money Showcase':
     teams = teams.unique()
     teams = sorted(teams)
 
-    teams = st.multiselect('Select Teams', teams)
+    teams = st.sidebar.multiselect('Select Teams', teams)
 
     filtered_data = filtered_data[(filtered_data['Away'].isin(teams)) | (filtered_data['Home'].isin(teams))]
 
     filtered_data['Time'] = pd.to_datetime(filtered_data['Date'] + '/24 ' + filtered_data['Time'])
     filtered_data = filtered_data[filtered_data['Time'] > pd.Timestamp.now()]
 
-    filtered_data = filtered_data[['Age Group', 'Away', 'Home', 'Time', 'Field']]
+    filtered_data = filtered_data[['Away', 'Home', 'Time', 'Field']]
 
     filtered_data['Time'] = filtered_data['Time'].dt.strftime('%m/%d/%Y %I:%M %p')
     filtered_data = filtered_data.sort_values(by='Time').drop_duplicates()
