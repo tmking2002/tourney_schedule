d16d4e69d0576a02f966c9775177a63a41c0c732
tmking2002 <tmking4@ncsu.edu>
2024-06-05 19:38:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ if tourney == 'PGF Show Me The Money Showcase':
 
     data = pd.read_csv('game_info.csv')
 
-    age_group = st.sidebar.multiselect('Select Age Group', data['Age Group'].unique())
+    default_age_group = ['U16', 'U18']
+
+    age_group = st.sidebar.multiselect('Select Age Group', data['Age Group'].unique(), default=default_age_group)
 
     filtered_data = data[data['Age Group'].isin(age_group)]
 
@@ -18,12 +20,32 @@ if tourney == 'PGF Show Me The Money Showcase':
     teams = teams.unique()
     teams = sorted(teams)
 
-    teams = st.sidebar.multiselect('Select Teams', teams)
+    default_teams = ['Athletics Gold Tamborra-Freeman', 'Atlanta Vipers 07- Maldonado', 'Birmingham Thunderbolts Premier 2025 Kemp', 'D1vision Softball 16U National', 
+                     'EC Bullets 18U Gold - Schnute', 'GA Bombers 16U National', 'GA Impact Taylor', 'GA Impact Premier - Maher', 'Impact - Caymol', 'Impact Caymol - Sullivan',
+                     'Hotshots National ATL', 'Impact Gold Jazz', 'Nationals - 16U - Sars', 'TAMPA MUSTANGS RENE 18U', 'Tampa Mustangs Seymour', 'Tri-State Thunder Gold',
+                     'Team North Carolina Bowman', 'Unity Meadows/Johnson', 'Birmingham Thunderbolts 18 Premier', 'Team NC Baylog / Tracy', 'Unity 16U - Slezak', 
+                     'Tennessee Mojo 2025-Gregory']
+
+    teams = st.sidebar.multiselect('Select Teams', teams, default=default_teams)
 
     filtered_data = filtered_data[(filtered_data['Away'].isin(teams)) | (filtered_data['Home'].isin(teams))]
 
     filtered_data['Time'] = pd.to_datetime(filtered_data['Date'] + '/24 ' + filtered_data['Time'])
     filtered_data = filtered_data[filtered_data['Time'] > pd.Timestamp.now()]
+
+    date_times = filtered_data['Time'].unique()
+    date_times = ["All"] + list(date_times)  # Add "All" to the list of options
+
+    # Display the multiselect widget
+    selected_date_times = st.sidebar.multiselect('Select Date/Time', date_times, default=["All"])
+
+    # Check if "All" is selected
+    if "All" in selected_date_times:
+        # If "All" is selected, show all date/times
+        filtered_data = filtered_data
+    else:
+        # Otherwise, filter the DataFrame based on selected date/times
+        filtered_data = filtered_data[filtered_data['Time'].isin(selected_date_times)]
 
     filtered_data = filtered_data[['Home', 'Away', 'Time', 'Field']]
 
