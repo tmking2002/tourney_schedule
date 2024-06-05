f74081bcd37614602fb54ff5da3bd2f0b02c3ba1
tmking2002 <tmking4@ncsu.edu>
2024-06-05 18:57:02 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+data = pd.read_csv('tourney_schedule/game_info.csv')
+
+age_group = st.multiselect('Select Age Group', data['Age Group'].unique())
+
+filtered_data = data[data['Age Group'].isin(age_group)]
+
+teams = pd.concat([filtered_data['Away'], filtered_data['Home']])
+teams = teams.unique()
+teams = sorted(teams)
+
+teams = st.multiselect('Select Team', teams)
+
+filtered_data = filtered_data[(filtered_data['Away'].isin(teams)) | (filtered_data['Home'].isin(teams))]
+filtered_data['Time'] = pd.to_datetime(filtered_data['Date'] + '/24 ' + filtered_data['Time'])
+filtered_data = filtered_data[['Age Group', 'Away', 'Home', 'Time', 'Field']]
+
+filtered_data['Time'] = filtered_data['Time'].dt.strftime('%m/%d/%Y %I:%M %p')
+filtered_data = filtered_data.sort_values(by='Time')
+
+if not filtered_data.empty:
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=list(filtered_data.columns),
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[filtered_data[col] for col in filtered_data.columns],
+                   fill_color='white',
+                   align='left'))
+    ])
+    
+    st.plotly_chart(fig)
+else:
+    st.write("No data available for the selected filters.")
---
+++ scrape_tourneymachine.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
+
+driver = webdriver.Chrome()
+
+def scrape_page(xpath, age_group):
+
+    url = 'https://tourneymachine.com/public/mobile/WebApp/index.aspx?IDTournament=h20240522143500344558863d64bd745'
+
+    driver.get(url)
+
+    divisions = driver.find_element(By.XPATH, '//*[@id="menuDivisions"]')
+    divisions.click()
+
+    page = driver.find_element(By.XPATH, xpath)
+    page.click()
+
+    # get html from page
+    html = driver.page_source
+
+    # create soup object
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # search for class col-xs-10
+    games = soup.find_all('div', class_='col-xs-10')
+    times = soup.find_all('div', class_='col-xs-2')
+    fields = soup.find_all('div', class_='scheduleLocation')
+
+    # create empty list to store game info
+    game_info = pd.DataFrame(columns=['Age Group', 'Game Number', 'Away', 'Home', 'Date', 'Time', 'Field'])
+
+    # loop through games and extract info
+    for game, time, field in zip(games, times, fields):
+        element_1 = game.find('h5', class_='scheduleTeam1')
+        element_2 = game.find('h5', class_='scheduleTeam2')
+
+        game_id = element_1['id']
+        game_num = game_id.split('-')[2]
+
+        team_1 = element_1.find('div').text
+        team_2 = element_2.find('div').text
+
+        element = time.find('h5')
+
+        date, time = element.get_text(separator='<br>').split('<br>')
+
+        field_element = field.find('p')
+        field = field_element.text
+
+        cur_info = pd.DataFrame([[age_group, game_num, team_1, team_2, date, time, field]], columns=['Age Group', 'Game Number', 'Away', 'Home', 'Date', 'Time', 'Field'])
+
+        game_info = pd.concat([game_info, cur_info])
+
+    return game_info
+
+u18 = scrape_page('//*[@id="divisions-team-31"]', 'U18')
+u16 = scrape_page('//*[@id="divisions-team-10"]', 'U16')
+u14 = scrape_page('//*[@id="divisions-team-1"]', 'U14')
+
+game_info = pd.concat([u18, u16, u14])
+
+game_info.to_csv('tourney_schedule/game_info.csv', index=False)
